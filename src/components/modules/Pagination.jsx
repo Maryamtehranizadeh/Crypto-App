0f1c38908b737579ffc899d05a1d32148522034a
Maryam Tehrani <maryamtehranizadeh@yahoo.com>
2024-09-25 17:16:20 +0100

    pagination

---
+++ src/components/modules/Pagination.jsx
@@ -0,0 +1,31 @@
+import { getCoinList } from "../../services/cryptoApi";
+
+function Pagination({ page, setPage }) {
+  const previousHandler = () => {
+    if (page <= 1) return;
+    setPage((page) => page - 1);
+  };
+  const nextHandler = () => {
+    if (page >= 10) return;
+    setPage((page) => page + 1);
+  };
+  return (
+    <div>
+      <button onClick={previousHandler}>Previous</button>
+      <p style={{ color: page === 1 ? "red" : "inherit" }}>1</p>
+      <p style={{ color: page === 2 ? "red" : "inherit" }}>2</p>
+      {page > 2 && page < 9 && (
+        <div>
+          <span>...</span> <p style={{ color: "red" }}>{page}</p>
+        </div>
+      )}
+      <span>...</span>
+
+      <p style={{ color: page === 9 ? "red" : "inherit" }}>9</p>
+      <p style={{ color: page === 10 ? "red" : "inherit" }}>10</p>
+      <button onClick={nextHandler}>Next</button>
+    </div>
+  );
+}
+
+export default Pagination;
--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,23 +1,27 @@
 import React, { useEffect, useState } from "react";
 import CoinTable from "../modules/CoinTable";
 import { getCoinList } from "../../services/cryptoApi";
+import Pagination from "../modules/Pagination";
 
 function HomePage() {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
+    setLoading(true);
     const fetchData = async () => {
-      const res = await fetch(getCoinList());
+      const res = await fetch(getCoinList(page));
       const json = await res.json();
       setCoins(json);
       setLoading(false);
     };
     fetchData();
-  }, []);
+  }, [page]);
   return (
     <div>
       <CoinTable coins={coins} loading={loading} />
+      <Pagination page={page} setPage={setPage} />
     </div>
   );
 }
