83eeee6daa0220b5e18bf8da384795c0216e4c5f
Maryam Tehrani <maryamtehranizadeh@yahoo.com>
2024-09-26 22:05:37 +0100

    finalized

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import styles from "./Layout.module.css";
 
 function Layout({ children }) {
   return (
     <>
-      <header>
+      <header className={styles.header}>
         <h1>Crypto App</h1>
         <p>
           <a href="https://botostart.ir">BotoStart</a> | React.js
         </p>
       </header>
       {children}
-      <footer>
+      <footer className={styles.footer}>
         <p>Developed By Mary with Love</p>
       </footer>
     </>
